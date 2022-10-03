5f56171747f155361e7ea8a0a109b91034fdd6fc
Fady Kamil <engfadykamil.2014@gmail.com>
2022-10-03 15:49:32 +0200

    Time: 3923 ms (5.15%), Space: 91.1 MB (50.47%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -4,18 +4,23 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
+    def my_gen(self, values):
+        for i, val in enumerate(values):
+            yield i, val
     def pairSum(self, head):
         """
         :type head: Optional[ListNode]
         :rtype: int
         """
         next_node = head
-        n = 0
         values = []
         while next_node != None:
-            n += 1
             values.append(next_node.val)
             next_node = next_node.next
-        twin_sums = [values[i] + values[n - 1 - i] for i, val in enumerate(values)]
-        return max(twin_sums)
+        n = len(values)
+        greatest_twin_sum = 0
+        for i, val in self.my_gen(values):
+            if values[i] + values[n - 1 - i] > greatest_twin_sum:
+                greatest_twin_sum = values[i] + values[n - 1 - i]
+        return greatest_twin_sum
         
